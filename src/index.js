b2ae6334fe725300729832dd56edfef2c6886725
Dj0nny <francesco.giambruno96@gmail.com>
2025-12-14 13:40:34 +0100

    Setup Sequelize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const dotenv = require('dotenv');
 
 const app = require('./app');
+const sequelize = require('./sequelize');
 
 dotenv.config();
 
 const port = process.env.PORT || 8050;
 
-app.listen(port, () => {
+app.listen(port, async () => {
 	try {
 		console.log(`🚀 Server's listening at: http://localhost:${port}.`);
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
 	} catch (error) {
 		console.error(`An error occurred: ${error}`);
 	}
